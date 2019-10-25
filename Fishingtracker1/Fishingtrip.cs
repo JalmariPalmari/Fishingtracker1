435e30f1be6b9ab8ce96cc22a174f7cd08f4b80b
Puura <Janne.Puura@seamk.fi>
2019-10-25 15:56:38 +0300

    Add project files.

---
+++ Fishingtracker1/Fishingtrip.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fishingtracker1
+{
+    class Fishingtrip
+    {
+        private string _place;
+        private bool _competition;
+        private string _competitionName;
+        private DateTime _date;
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private string _crewName;
+
+        public Fishingtrip(string place, bool competition, string competitionName, DateTime date, DateTime startTime, string crewName)
+        {
+            _place = place;
+            _competition = competition;
+            _competitionName = competitionName;
+            _date = date;
+            _startTime = startTime;
+            _crewName = crewName;
+
+        }
+    }
+}
---
+++ Fishingtracker1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fishingtracker1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
