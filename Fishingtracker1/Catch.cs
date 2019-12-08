2069afa71263addce29a3fec8411003a6b55d0c5
JalmariPalmari <puura.janne@gmail.com>
2019-12-08 23:36:51 +0200

    Versio 08.12

--- Fishingtracker1/Catch.cs
+++ Fishingtracker1/Catch.cs
@@ -3,27 +3,36 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Linq;
+using NpgsqlTypes;
 
 namespace Fishingtracker1
 {
    
     class Catch
     {
-        private string _fish;
+        private int _kalalajinvalinta;
         private int _weight;
         private int _lenght;
+        private string _fish;
+        private NpgsqlDateTime _fishtime;
        private static int _fishCount;
-        
 
+        // lista kilogrammojen tallentamista varten.
         private List<int> kgsumma = new List<int>();
-        public Catch(string fish, int weight, int lenght, DateTime fishtime)
+        public Catch(int kalalajinvalinta, string fish, int weight, int lenght, NpgsqlDateTime fishtime)
         {
 
+            _kalalajinvalinta = kalalajinvalinta;
             _fish = fish;
             _lenght = lenght;
             _weight = weight;
+            _fishtime = fishtime;
             kgsumma.Add(weight);
             _fishCount++;
+        }
+        public int GetFishNumber()
+        {
+            return _kalalajinvalinta;           
         }
         public string GetFishSpecies()
         {
@@ -40,6 +49,10 @@ namespace Fishingtracker1
         public int GetFishCount()
         {
             return _fishCount;
+        }
+        public NpgsqlDateTime GetFishTime()
+        {
+            return _fishtime;
         }
         public int GetWeightSum()
         {
